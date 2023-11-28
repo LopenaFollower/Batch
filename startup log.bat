f91c1733d1d0ebe11cb98424a9263bc15048e137
LopenaFollower <115658149+LopenaFollower@users.noreply.github.com>
2023-11-28 10:14:43 +0800

    Add files via upload

--- startup log.bat
+++ startup log.bat
@@ -1,2 +1,2 @@
 @echo off
-Powershell -command " $hookUrl = 'https://discord.com/api/webhooks/1074048322744090714/nMHd93KgJrP5lFC94Hmp1xaPVmxyOSG2GHS91SB7GK98W_Gb3-13l9U7e21xOCl7D84f'; $content = hostname +' Logged on at '+ (Get-Date); $payload = [PSCustomObject]@{content = $content}; Invoke-RestMethod -Uri $hookUrl -ContentType 'Application/Json' -Method Post -Body ($payload | ConvertTo-Json); "
+Powershell -command " $hookUrl = 'https://discord.com/api/webhooks/1074048322744090714/nMHd93KgJrP5lFC94Hmp1xaPVmxyOSG2GHS91SB7GK98W_Gb3-13l9U7e21xOCl7D84f'; $content = ($env:COMPUTERNAME) +' Logged on at '+ (Get-Date); $payload = [PSCustomObject]@{content = $content}; Invoke-RestMethod -Uri $hookUrl -ContentType 'Application/Json' -Method Post -Body ($payload | ConvertTo-Json); "
